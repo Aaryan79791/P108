06d3310ed3fb79135c1f46c6548de3c6169896aa
Aaryan79791 <99226366+Aaryan79791@users.noreply.github.com>
2023-09-11 17:55:26 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({audio: true, video:false});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/bXuZ90IBU/model.json', { probabiltyThreshold: 0.7 } ,modelReady);
+}
+
+function modelReady(){
+    classifier.classify(gotResults);
+}
+
+var dog = 0;
+var cat = 0;
+var cow = 0;
+var t_rex = 0;
+
+function gotResults(error, results){
+    if (error) {
+    console.error(error);
+    } else
+    console.log(results);
+    random_number_r = Math.floor(Math.random() * 255) + 1;
+    random_number_g = Math.floor(Math.random() * 255) + 1;
+    random_number_b = Math.floor(Math.random() * 255) + 1;
+
+    document.getElementById("result_label").innerHTML = 'Detected voice is of  - '+ results[0].label;
+    document.getElementById("result_count").innerHTML = 'Detected Dog - '+dog+ ' Detected Cat - '+cat+ 'Detected Cow - '+cow+ 'Detected T-Rex -'+t_rex;
+    document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+    document.getElementById("result_count").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+
+    img = document.getElementById('animal_image');
+
+    if (results[0].label == "Barking") {
+    img.src = "Dog_Gif.gif";
+    dog = dog+1;
+
+    }else if (results[0].label == "Meowing"){
+    img.src = "Cat_Gif.gif";
+    cat = cat+1;
+
+    }else if (results[0].label == "Mooing"){
+    img.src = "Cow_Gif.gif.";
+    cow = cow+1;
+    
+    }else if (results[0].label == "Roaring"){
+    img.src = "T-Rex_Gif.gif";
+    t_rex = t_rex+1;
+    
+    }else{
+    img.src = "ear_image.png";
+    }
+}
